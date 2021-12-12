4267fb5e6ad194aae6b251ee95a9ece14faa4103
PoomPK <poompk2542@gmail.com>
2021-12-12 13:15:04 +0700

    Guess The Number Game

--- lib/test.dart
+++ lib/test.dart
@@ -30,7 +30,7 @@ void main() {
         break;
       } else {
 
-        // thedistance betweet the input number and the answer
+        // the distance betweet the input number and the answer
         num range = sqrt((pow(guessNum-answer, 2)).abs());
 
         /**
